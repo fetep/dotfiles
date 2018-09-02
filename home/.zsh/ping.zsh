c7c18f3a31bd0774bd576769cdbb89bad5cb436a
Pete Fritchman <petef@databits.net>
2018-09-02 18:10:22 +0000

    ping helper

---
+++ home/.zsh/ping.zsh
@@ -0,0 +1,28 @@
+function pingw() {
+  local last_state="_"
+  local state
+
+  if [[ $# -ne 1 ]]; then
+    echo "usage: pingw <host>" >&2
+    return 1
+  fi
+
+  host=$1
+  if ! getent hosts $host >/dev/null 2>&1; then
+    echo "pingw: $host: not found" >&2
+    return 2
+  fi
+
+  while :; do
+    if fping -q $host; then
+      state="up"
+    else
+      state="down"
+    fi
+    if [[ $state != $last_state ]]; then
+      echo "$(date) $host $state"
+    fi
+    last_state=$state
+    sleep 1
+  done
+}
