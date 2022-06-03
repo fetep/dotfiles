76fe69279af1d2c20065b5e08735bb58bc9216ee
Pete Fritchman <petef@databits.net>
2022-06-03 01:17:28 +0000

    pass -- so options go to the command, not bazel

--- home/.zsh/bazel.zsh
+++ home/.zsh/bazel.zsh
@@ -1,2 +1,2 @@
 alias bb="bazel build"
-alias br="bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress"
+alias br="bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress --"
