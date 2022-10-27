5b4f24593d417724f80e6aaba5b8162da1faa7fc
Pete Fritchman <petef@databits.net>
2022-10-27 15:55:30 +0000

    better bazel alias

--- home/.zsh/bazel.zsh
+++ home/.zsh/bazel.zsh
@@ -1,2 +1,25 @@
-alias bb="bazel build"
-alias br="bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress --"
+alias br="b r"
+
+b() {
+  local cmd=$1
+  shift
+
+  case $cmd in
+    b|build)
+      bazel build "$@"
+      ;;
+    r|run)
+      bazel run --ui_event_filters=-info,-stdout,-stderr --noshow_progress -- "$@"
+      ;;
+    t|test)
+      if [[ "$@" == "" ]]; then
+        bazel test --test_output=errors //...
+      else
+        bazel test --test_output=errors "$@"
+      fi
+      ;;
+    *)
+      bazel "$@"
+      ;;
+  esac
+}
