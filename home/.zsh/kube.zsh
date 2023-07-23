7281c1b1967091df4b3c14af373bda9271501d9d
Pete Fritchman <petef@databits.net>
2023-07-23 15:47:24 -0400

    better kube functions

--- home/.zsh/kube.zsh
+++ home/.zsh/kube.zsh
@@ -1,66 +1,14 @@
-# k: set kube namespace for current context
-# also toggles kubectl context details in prompt
-function _k_usage() {
-  echo "Usage: k -l"
-  echo "       k namespace"
-  echo "       k"
-  echo ""
-  echo "-l         List all available namespaces in current context"
-  echo "namespace  New namespace to switch to"
-  echo ""
-  echo "Calling k without arguments will toggle displaying current context/namespace in prompt"
-}
-
-# list namespaces
-function _k_list() {
-  kubectl -o 'name' get namespaces | cut -d/ -f2-
-  return $pipestatus[1]
-}
-
-# autocomplete for 'k'
-function _k() {
-  _arguments "-l" "-h"
-  _alternative "ns:namespace:($(_k_list))"
-}
-
+# k: kubectl shortcut. no arguments toggles prompt info
 function k() {
-  while getopts "hl" opt; do
-    case "$opt" in
-      h)
-        _k_usage
-        return 0
-        ;;
-      l)
-        _k_list
-        return $?
-        ;;
-      *)
-        _k_usage >&2
-        return 1
-    esac
-  done
-  shift $((OPTIND-1))
-
-  if [[ $# > 1 ]]; then
-    echo "k: takes at most one argument (namespace)" >&2
-    _k_usa
-    return 1
-  elif [[ $# == 0 ]]; then
-    # if run with no arguments, toggle prompt on/off
-    if [[ -n "$_kube_prompt" ]]; then
-      unset _kube_prompt
-    else
-      _kube_prompt=1
-    fi
-    return
+  # if run with no arguments, toggle prompt on/off
+  if [[ $# -eq 0 ]]; then
+    [[ -n "$_kube_prompt" ]] && unset _kube_prompt || _kube_prompt=1
+    return 0
   fi
 
   _kube_prompt=1
-  kubectl config set-context --current --namespace="$1" >/dev/null
-  return $?
+  \kubectl "$@"
 }
-
-compdef _k k
 
 # kc: set kube context
 function _kc_usage() {
@@ -73,7 +21,7 @@ function _kc_usage() {
 
 # autocomplete for 'kc'
 function _kc() {
-  _arguments "-l" "-h"
+  _arguments "-l"
   _alternative "c:context:($(kubectl config get-contexts -o name))"
 }
 
@@ -108,6 +56,58 @@ function kc() {
 
 compdef _kc kc
 
+# kn: set kube namespace for current context
+## kn autocomplete
+function _kn() {
+  _arguments "-l"
+  _alternative "ns:namespace:($(_kn_list))"
+}
+
+function _kn_list() {
+  kubectl -o 'name' get namespaces | cut -d/ -f2-
+  return $pipestatus[1]
+}
+
+function _kn_usage() {
+  echo "Usage: kn -l"
+  echo "       kn namespace"
+  echo ""
+  echo "-l         List all available namespaces in current context"
+  echo "namespace  New namespace to switch to"
+}
+
+function kn() {
+  while getopts "hl" opt; do
+    case "$opt" in
+      h)
+        _kn_usage
+        return 0
+        ;;
+      l)
+        _kn_list
+        return $?
+        ;;
+      *)
+        echo "kn: $opt: unknown option" >&2
+        _kn_usage >&2
+        return 1
+    esac
+  done
+  shift $((OPTIND-1))
+
+  if [[ $# != 1 ]]; then
+    echo "kn: must pass exactly one argument (namespace)" >&2
+    _kn_usage
+    return 1
+  fi
+
+  _kube_prompt=1
+  kubectl config set-context --current --namespace="$1" >/dev/null
+  return $?
+}
+
+compdef _kn kn
+
 # prompt hook to set _kube_context, used by prompt.zsh
 add-zsh-hook precmd update-kube-vars
 
@@ -122,3 +122,6 @@ for tool in flux helm kubectl kustomize; do
     source <("$tool" completion zsh)
   fi
 done
+
+# k alias
+compdef k=kubectl
