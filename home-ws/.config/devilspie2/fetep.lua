399a1b771c883305eb74877e37aac75f83745d96
Pete Fritchman <petef@databits.net>
2023-07-23 15:47:11 -0400

    devilspie2 config update

--- home-ws/.config/devilspie2/fetep.lua
+++ home-ws/.config/devilspie2/fetep.lua
@@ -9,8 +9,12 @@ elseif class == "alacritty" and instance == "misc" then set_window_workspace(4)
 elseif name == "firefox" then set_window_workspace(2)
 elseif class == "discord" then set_window_workspace(3)
 elseif class == "slack" then set_window_workspace(3)
-elseif class == "signal" then set_window_workspace(3)
-elseif class == "telegram" then set_window_workspace(3)
+elseif class == "signal" then
+  set_window_workspace(3)
+  minimize()
+elseif class == "telegramdesktop" then
+  set_window_workspace(3)
+  minimize()
 elseif name == "keepassxc" then set_window_workspace(4)
 else print(string.format("Unmapped window name=%s class=%s", name, class))
 end
