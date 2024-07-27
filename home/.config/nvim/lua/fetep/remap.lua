990162b6b924ec63e49f048b5e80cbb237412cac
Pete Fritchman <petef@databits.net>
2024-07-27 21:38:50 +0000

    drop archived toggle-lsp-diganostics module

--- home/.config/nvim/lua/fetep/remap.lua
+++ home/.config/nvim/lua/fetep/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = ','
 
+-- control line numbers
 vim.keymap.set('n', '<leader>1', function()
     vim.opt.number = not(vim.opt.number:get())
 end)
@@ -7,12 +8,32 @@ vim.keymap.set('n', '<leader>2', function()
     vim.opt.relativenumber = not(vim.opt.relativenumber:get())
 end)
 
+-- file navigation
 vim.keymap.set('n', '<leader>ls', vim.cmd.Ex)
+
+-- remove highlights
 vim.keymap.set('n', '<leader>m', vim.cmd.noh)
+
+-- toggle paste mode
 vim.keymap.set('n', '<leader>p', function()
     vim.opt.paste = not(vim.opt.paste:get())
 end)
 
+-- toggle diagnostics, default to off
+vim.g.diagnostics_enabled = false
+vim.diagnostic.hide()
+vim.keymap.set('n', '<leader>d', function()
+    if vim.g.diagnostics_enabled then
+        vim.g.diagnostics_enabled = false
+        vim.diagnostic.hide()
+    else
+        vim.g.diagnostics_enabled = true
+        vim.diagnostic.show()
+    end
+    print(string.format('diagnostics_enabled=%s', vim.g.diagnostics_enabled))
+end)
+
+-- toggle line 100 column highlight
 function _G.toggle_colorcolumn()
     local value = vim.api.nvim_get_option_value('colorcolumn', {})
     if value == '' then
