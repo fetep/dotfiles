72b045436d42fe8a80c4ce9df7095a1adf92a6a1
Pete Fritchman <petef@databits.net>
2024-07-30 03:50:24 +0000

    go + autofmt

---
+++ home/.config/nvim/lua/fetep/plugins/go.lua
@@ -0,0 +1,25 @@
+-- https://github.com/ray-x/go.nvim
+-- go enhancements
+return {
+    'ray-x/go.nvim',
+        dependencies = {
+            'ray-x/guihua.lua',
+            'neovim/nvim-lspconfig',
+            'nvim-treesitter/nvim-treesitter',
+        },
+        config = function()
+            require('go').setup()
+
+            local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                pattern = '*.go',
+                callback = function()
+                    require('go.format').goimports()
+                end,
+                group = format_sync_grp,
+            })
+        end,
+        event = {'CmdlineEnter'},
+        ft = {'go', 'gomod'},
+        build = ':lua require("go.install").update_all_sync()'
+}
