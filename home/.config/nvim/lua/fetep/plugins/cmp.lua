bbf4e1a49df1f5d763dac363281c635e18560020
Pete Fritchman <petef@databits.net>
2025-10-12 23:45:18 +0000

    nvim: fix up cmp plugin config, try autopairs

--- home/.config/nvim/lua/fetep/plugins/cmp.lua
+++ home/.config/nvim/lua/fetep/plugins/cmp.lua
@@ -6,39 +6,39 @@ return {
         'hrsh7th/nvim-cmp',
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
+            --'hrsh7th/cmp-nvim-lsp-document-symbol',
             'mason-org/mason.nvim',
             'mason-org/mason-lspconfig.nvim',
         },
-        ft = {
-            -- install LSPs in mason-lspconfig.lua & configure below
-            'bash',
-            'go',
-            'lua',
-            'python',
-            'rust',
-            'terraform',
-            'yaml',
+        opts = {
         },
         config = function()
             local cmp = require('cmp')
-            local cmp_select = {
-                behavior = cmp.SelectBehavior.Select
-            }
 
-            cmp.setup {
-                mapping = cmp.mapping.preset.insert({
-                    ['<Tab>'] = cmp.mapping.complete(),
+            cmp.setup({
+                enabled = function()
+                    local disabled = false
+                    disabled = disabled or (vim.api.nvim_get_option_value('buftype', { buf = 0 }) == 'prompt')
+                    disabled = disabled or require('cmp.config.context').in_treesitter_capture('comment')
+                    return not disabled
+                end,
+                mapping = {
+                    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-c>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true })
-                }),
+                    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+                    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+                },
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end,
+                },
                 sources = {
-                    { name = 'nvim_lsp' }
-                }
-            }
-        end,
+                    { name = 'nvim_lsp' },
+                },
+            })
+        end
     },
 }
--- home/.config/nvim/lua/fetep/plugins/lspconfig.lua
+++ home/.config/nvim/lua/fetep/plugins/lspconfig.lua
@@ -11,6 +11,7 @@ return {
         -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
         config = function()
             -- no need to call vim.lsp.enable() here, mason-lspconfig takes care of that
+            -- pass capabilities in every lsp config to know about nvim-cmp completions
             local capabilities = require('cmp_nvim_lsp').default_capabilities();
 
             vim.lsp.config('bashls', {
